b1b4d9c5d9a22b8d9cdfb330d04ae17f0485d76c
Lee JaeJun <monooi@naver.com>
2024-01-31 17:03:02 +0900

    Add: Add and remove tab info to chrome storage

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,27 @@
-console.log("background");
+chrome.tabs.onCreated.addListener((tab) => {
+  const tabInfo = {
+    id: tab.id,
+    title: tab.title,
+    url: tab.url,
+    createdAt: Date.now(),
+  };
+
+  chrome.storage.local.set({ [tab.id as number]: tabInfo });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.title || changeInfo.url) {
+    const tabInfo = {
+      id: tabId,
+      title: tab.title,
+      url: tab.url,
+      createdAt: Date.now(),
+    };
+
+    chrome.storage.local.set({ [tabId]: tabInfo });
+  }
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  chrome.storage.local.remove(tabId.toString());
+});
